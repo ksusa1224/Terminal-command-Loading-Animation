cbdd40bfb9a190e2c490b5934344b640297f6b6f
Apple Buster <ksusa1224@gmail.com>
2016-06-21 23:17:46 +0900

    サンプルDB作成
    指定したフォルダにサンプルDB作成しただけ

--- src/main/java/com/application/AnkiNoteApplication.java
+++ src/main/java/com/application/AnkiNoteApplication.java
@@ -1,5 +1,11 @@
 package com.application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +15,50 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AnkiNoteApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+	    // load the sqlite-JDBC driver using the current class loader
+		Class.forName("org.sqlite.JDBC");
+
+	    Connection connection = null;
+	    try
+	    {
+	      // create a database connection
+	      connection = DriverManager.getConnection("jdbc:sqlite:sqlite_databases/ver1/sample.db");
+	      Statement statement = connection.createStatement();
+	      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+	      statement.executeUpdate("drop table if exists person");
+	      statement.executeUpdate("create table person (id integer, name string)");
+	      statement.executeUpdate("insert into person values(1, 'leo')");
+	      statement.executeUpdate("insert into person values(2, 'yui')");
+	      ResultSet rs = statement.executeQuery("select * from person");
+	      while(rs.next())
+	      {
+	        // read the result set
+	        System.out.println("name = " + rs.getString("name"));
+	        System.out.println("id = " + rs.getInt("id"));
+	      }
+	    }
+	    catch(SQLException e)
+	    {
+	      // if the error message is "out of memory", 
+	      // it probably means no database file is found
+	      System.err.println(e.getMessage());
+	    }
+	    finally
+	    {
+	      try
+	      {
+	        if(connection != null)
+	          connection.close();
+	      }
+	      catch(SQLException e)
+	      {
+	        // connection close failed.
+	        System.err.println(e);
+	      }
+	    }
+
 		SpringApplication.run(AnkiNoteApplication.class, args);
 	}
 }
