288a1eff127bfa5a652be6ef451b0678172bb580
Apple Buster <ksusa1224@gmail.com>
2016-06-30 08:03:29 +0900

    SQLiteの自動生成ブランチ完成

--- src/main/java/com/application/AnkiNoteApplication.java
+++ src/main/java/com/application/AnkiNoteApplication.java
@@ -1,22 +1,12 @@
 package com.application;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import com.dao.*;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@ComponentScan({ "com.application.services", "com.controller.controllers" })
 public class AnkiNoteApplication {
 
 	public static void main(String[] args) {
