a241b76b918412eb8fe47722deb02d9cb49fb432
Apple Buster <ksusa1224@gmail.com>
2016-07-22 09:27:44 +0900

    メインページのCSS読み込まれるようにした
    htmlと同名のCSSだとだめみたい。動作はOKだけど、ソース未整理。不要なコピペもととかいる

--- src/main/java/com/application/AnkiNoteApplication.java
+++ src/main/java/com/application/AnkiNoteApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
@@ -14,8 +19,30 @@ public class AnkiNoteApplication {
 //	public class AnkiNoteApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+//		SpringApplication app = new SpringApplication(AnkiNoteApplication.class);
+//	    System.out.print("Starting app with System Args: [" );
+//	    for (String s : args) {
+//	      System.out.print(s + " ");
+//	    }
+//	    System.out.println("]");
+//	    app.run(args);
 		SpringApplication.run(AnkiNoteApplication.class, args);
 	}
+
+//	@Bean
+//	public ViewResolver viewResolver() {
+//	    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+//	    templateResolver.setTemplateMode("XHTML");
+//	    templateResolver.setPrefix("views/");
+//	    templateResolver.setSuffix(".html");
+//
+//	    SpringTemplateEngine engine = new SpringTemplateEngine();
+//	    engine.setTemplateResolver(templateResolver);
+//
+//	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//	    viewResolver.setTemplateEngine(engine);
+//	    return viewResolver;	
+//	}
 	
 //	@Autowired JdbcTemplate jdbc;
 //	
--- src/main/java/com/application/controller/ApplicationController.java
+++ src/main/java/com/application/controller/ApplicationController.java
@@ -1,9 +1,14 @@
 package com.application.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -37,6 +42,18 @@ public class ApplicationController  {
 	public String index(HttpServletRequest request) {
 		String request_url = request.getRequestURI();
 		String response_url = "/index.html";
+
+		String filepath = "static/css/index.css"; // src/main/resources 配下の相対パス
+        Resource resource = resourceLoader.getResource("classpath:" + filepath);
+        try {
+			File file = resource.getFile();
+			System.out.println("----------------------");
+			System.out.println(file.getAbsolutePath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		if (request_url.equals(response_url))
 		{
 			return "index";
@@ -47,6 +64,9 @@ public class ApplicationController  {
 		}
 	}
 
+	@Autowired
+    ResourceLoader resourceLoader;
+	
 	/**
 	 * メインページ（暗記ノート本体）
 	 * http://blog.codebook-10000.com/entry/20140301/1393628782
@@ -57,17 +77,33 @@ public class ApplicationController  {
 						   "/{user_id}/", 
 						   "/{user_id}/main.html", 
 						   "/{user_id}/main.htm", 
-						   "/{user_id}/main"},
-							method=RequestMethod.GET)
-	public String main(@PathVariable("user_id") String user_id,HttpServletRequest request) {
+						   "/{user_id}/main"})
+	public String main(@PathVariable("user_id") String user_id,
+						HttpServletRequest request) {
 		
 		String request_url = request.getRequestURI();
 		String response_url = "/"+ user_id + "/main.html";
+		System.out.println("----------------------");
+		System.out.println(request_url);
 		
+//		String path = "";
+		
+		String filepath = "static/css/main.css"; // src/main/resources 配下の相対パス
+        Resource resource = resourceLoader.getResource("classpath:" + filepath);
+//        try {
+//			File file = resource.getFile();
+//			System.out.println("----------------------");
+//			System.out.println(file.getAbsolutePath());
+//			//path = file.getAbsolutePath();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+        
 		// TODO セッションから取得
 		// TODO ログインチェック
 		// TODO DBに存在するかチェック
-		if(user_id.equals("vsky") || user_id.equals("miho"))		
+		if(user_id.equals("vsky"))		
 		{
 			if (request_url.equals(response_url))
 			{
@@ -77,10 +113,22 @@ public class ApplicationController  {
 			{
 				return "redirect:" + response_url;
 			}
-		}
+		}		
 		else
 		{
 			return "error";
+//			return "redirect:../css/main2.css";
+//			return "redirect:" + "/css/main.css";
+//			return "../css/main.css";
 		}
+//		else if (request_url.equals("/css/main.css"))
+//		{
+//			return path;			
+//		}
+//		else
+//		{
+////			return "error";
+//			return path;
+//		}
 	}	    
 }
