142ce8342a90bfa0092405927c9df2f44e718a9f
Apple Buster <ksusa1224@gmail.com>
2016-07-22 09:44:15 +0900

    ソースの整理
    使っていないソースを全体的に削除。動作確認OK

--- src/main/java/com/application/AnkiNoteApplication.java
+++ src/main/java/com/application/AnkiNoteApplication.java
@@ -1,54 +1,15 @@
 package com.application;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-//import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AnkiNoteApplication {
-//	public class AnkiNoteApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-//		SpringApplication app = new SpringApplication(AnkiNoteApplication.class);
-//	    System.out.print("Starting app with System Args: [" );
-//	    for (String s : args) {
-//	      System.out.print(s + " ");
-//	    }
-//	    System.out.println("]");
-//	    app.run(args);
 		SpringApplication.run(AnkiNoteApplication.class, args);
-	}
-
-//	@Bean
-//	public ViewResolver viewResolver() {
-//	    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-//	    templateResolver.setTemplateMode("XHTML");
-//	    templateResolver.setPrefix("views/");
-//	    templateResolver.setSuffix(".html");
-//
-//	    SpringTemplateEngine engine = new SpringTemplateEngine();
-//	    engine.setTemplateResolver(templateResolver);
-//
-//	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//	    viewResolver.setTemplateEngine(engine);
-//	    return viewResolver;	
-//	}
-	
-//	@Autowired JdbcTemplate jdbc;
-//	
-//	// アプリ起動時に実行される。
-//    @Override public void run(String... args) throws Exception {
-//        jdbc.execute("create table task (id serial, name varchar(255))");
-//        jdbc.update("insert into task (name) values (?)", "First Task.");
-//    }	
+	}	
 }
--- src/main/java/com/application/controller/ApplicationController.java
+++ src/main/java/com/application/controller/ApplicationController.java
@@ -1,33 +1,14 @@
 package com.application.controller;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.dao.SQliteDAO;
-import com.application.*;
-import com.application.model.TopPageModel;
 
 @Controller
-public class ApplicationController  {
+public class ApplicationController {
 	
 	/**
 	 * トップページ
@@ -42,17 +23,6 @@ public class ApplicationController  {
 	public String index(HttpServletRequest request) {
 		String request_url = request.getRequestURI();
 		String response_url = "/index.html";
-
-		String filepath = "static/css/index.css"; // src/main/resources 配下の相対パス
-        Resource resource = resourceLoader.getResource("classpath:" + filepath);
-        try {
-			File file = resource.getFile();
-			System.out.println("----------------------");
-			System.out.println(file.getAbsolutePath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		if (request_url.equals(response_url))
 		{
@@ -63,12 +33,11 @@ public class ApplicationController  {
 			return "redirect:" + response_url;
 		}
 	}
-
-	@Autowired
-    ResourceLoader resourceLoader;
 	
 	/**
 	 * メインページ（暗記ノート本体）
+	 * 
+	 * Spring MVC　@PathVariableを使ってURLに含まれる動的なパラメータを取得
 	 * http://blog.codebook-10000.com/entry/20140301/1393628782
 	 * @param user_id
 	 * @return
@@ -83,27 +52,11 @@ public class ApplicationController  {
 		
 		String request_url = request.getRequestURI();
 		String response_url = "/"+ user_id + "/main.html";
-		System.out.println("----------------------");
-		System.out.println(request_url);
-		
-//		String path = "";
-		
-		String filepath = "static/css/main.css"; // src/main/resources 配下の相対パス
-        Resource resource = resourceLoader.getResource("classpath:" + filepath);
-//        try {
-//			File file = resource.getFile();
-//			System.out.println("----------------------");
-//			System.out.println(file.getAbsolutePath());
-//			//path = file.getAbsolutePath();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-        
+		   
 		// TODO セッションから取得
 		// TODO ログインチェック
 		// TODO DBに存在するかチェック
-		if(user_id.equals("vsky"))		
+		if(user_id.equals("vsky") || user_id.equals("miho"))		
 		{
 			if (request_url.equals(response_url))
 			{
@@ -117,18 +70,6 @@ public class ApplicationController  {
 		else
 		{
 			return "error";
-//			return "redirect:../css/main2.css";
-//			return "redirect:" + "/css/main.css";
-//			return "../css/main.css";
 		}
-//		else if (request_url.equals("/css/main.css"))
-//		{
-//			return path;			
-//		}
-//		else
-//		{
-////			return "error";
-//			return path;
-//		}
 	}	    
 }
