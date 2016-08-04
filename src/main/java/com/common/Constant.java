fae5f7d948d14ac638c48a181867e60f78767cda
ksusa1224 <ksusa1224@gmail.com>
2016-08-04 20:08:16 +0900

    Win用にパスを書き換え
    Macで使うときはコメントアウト部分を変更する必要あり

--- src/main/java/com/common/Constant.java
+++ src/main/java/com/common/Constant.java
@@ -1,6 +1,8 @@
 package com.common;
 
 public class Constant {
+	// MAC
+	/* 
 	public static final String ANKI_NOTE_ROOT_PATH = "/usr/local/anki_note/";
 	public static final String ANKI_NOTE_FOLDER_PASSWORD = "Xdsjiafr8FSfjIORFJSIOE+U";
 	public static final String DB_FOLDER_PATH = "/usr/local/anki_note/databases/";
@@ -8,4 +10,13 @@ public class Constant {
 	public static final String SQLITE_FOLDER_PATH = "/usr/local/anki_note/databases/sqlite/";
 	public static final String SQLITE_LOG_FOLDER_PATH = "/usr/local/anki_note/databases/sqlite/data/log_db/";
 	public static final String SQLITE_USER_DB_FOLDEDR_PATH = "/usr/local/anki_note/databases/sqlite/data/user_db/";
+	*/
+	// WINDOWS
+	public static final String ANKI_NOTE_ROOT_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\";
+	public static final String ANKI_NOTE_FOLDER_PASSWORD = "Xdsjiafr8FSfjIORFJSIOE+U";
+	public static final String DB_FOLDER_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\databases\\";
+	public static final String H2DB_FOLDER_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\databases\\h2db\\";
+	public static final String SQLITE_FOLDER_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\databases\\sqlite\\";
+	public static final String SQLITE_LOG_FOLDER_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\databases\\sqlite\\data\\log_db\\";
+	public static final String SQLITE_USER_DB_FOLDEDR_PATH = "C:\\Users\\ksusa\\Documents\\作業用\\ank\\databases\\sqlite\\data\\user_db\\";
 }
