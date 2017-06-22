62f517c606117e5ba12db9a70d1bab9a4728873d
ksusa <ksusa1224@gmail.com>
2017-06-22 13:45:25 +0900

    問題集ページ新規作成

---
+++ src/main/java/com/application/controller/WorkbookController.java
@@ -0,0 +1,24 @@
+package com.application.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.application.service.WorkbookService;
+
+@Controller
+public class WorkbookController {
+
+	@Autowired
+	private WorkbookService workbookService;
+	
+	@RequestMapping(value={"/workbook.html"}, method=RequestMethod.GET)
+	public String workbook(HttpSession session)
+	{
+		String owner_id = (String)session.getAttribute("owner_id");		
+		return "workbook";
+	}
+}
